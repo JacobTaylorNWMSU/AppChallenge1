9f9118136c7f193ddb1a39f25c1aa1accfbc20fd
JacobTaylorNWMSU <s526858@nwmissouri.edu>
2019-09-30 11:22:18 -0500

    forgot to add .js file to last commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const area = (x, y) => { return x * y }
+
+const updateWithArea = async (event) => {
+    document.querySelector('#result').innerHTML = ''
+    if (document.querySelector('#width').checkValidity() && document.querySelector('#length').checkValidity()) {
+      const regex = /[^a-zA-Z_]/g
+      const i = parseInt(document.querySelector('#width').value)
+      const j = parseInt(document.querySelector('#length').value)
+      const ans = `Your area is ${area(i, j)}.`
+      document.querySelector('#result').innerHTML = ans
+    }
+  }
+
+document.addEventListener('click', event => {
+    if (event.target && event.target.id === 'calculate') { updateWithArea(event) }
+  })
